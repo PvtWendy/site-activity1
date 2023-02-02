4f544f180f60f291040dc585d6551187822baa3c
Wendy Hemlock <wendyhemlock@gmail.com>
2023-02-01 21:33:21 -0400

    Almost finished News section

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 First react website that i'm fully commited to finish.
+This is only optimized for 1920x1080 resolution for now
 Design and images taken from: https://html.design/download/pullo-shoes-shop-template/
--- src/components/home/News.js
+++ src/components/home/News.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "../../style/News.css";
 import newsshoe from "../../images/about-img1.png";
-import newsshoe2 from  "../../images/shoes-img2.png";
+import newsshoe2 from "../../images/shoes-img2.png";
+import runningshoes from "../../images/shoes-img3.png";
 import newsbg from "../../images/about-img2.png";
+
 
 export default function News() {
   return (
@@ -15,17 +17,56 @@ export default function News() {
           exercitation
         </p>
       </div>
+
       <div className="NewsCollection">
         <p className="NewsNew">New</p>
         <div class="NewsShoe1">
           <img src={newsshoe}></img>
+          <div className="Description">
+            <p>Men Sports</p>
+            <p>$90</p>
+            <div id="Stars1"></div>
+          </div>
         </div>
         <div className="NewsShoe2">
-          <img src={newsbg} ></img>
-          <img src={newsshoe2} id="NewShoeImg"></img>
+          <img src={newsbg}></img>
+          <img src={newsshoe2}></img>
+          <div id="Description2">
+            <p>Men Sports</p>
+            <p>$90</p>
+            <div id="Stars2"></div>
+          </div>
         </div>
+        <p id="See">See More</p>
+      </div>
 
-        <img></img>
+      <div className="NewsText">
+        <h1>Racing Boot</h1>
+        <p>
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud
+          exercitation
+        </p>
+      </div>
+
+      <div className="RunningShoes">
+        <img src={runningshoes}></img>
+        <div className="RunningShoesText">
+          <h1>Sale</h1>
+          <h2>JOGGING</h2>
+          <h1>SHOES</h1>
+          <h3>$100</h3>
+          <p>See More</p>
+        </div>
+      </div>
+
+      <div className="NewsText">
+        <h1>News Arrivals Products</h1>
+        <p>
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud
+          exercitation
+        </p>
       </div>
     </>
   );
